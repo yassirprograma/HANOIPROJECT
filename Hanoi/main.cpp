fe796c6584b4a0d044a0d31c4f5f8bd54605daba
yassirprograma <dj.jicara@gmail.com>
2020-03-31 00:23:27 -0600

    Se construyó la pantalla gráfica y se situaron los lugares correspondientes a las torres

--- Hanoi/main.cpp
+++ Hanoi/main.cpp
@@ -1,17 +1,48 @@
 #include <stdio.h>
-
+#include <winbgim.h>
 
 int hanoi(int, char, char, char);
 
-main (){
+int main (){
 
 int ndiscos, movimientos;
 
 printf("N de discos: ");scanf("%d",&ndiscos);
 movimientos = hanoi(ndiscos,'A','B','C');
 
+initwindow(1080,500);
+    setbkcolor(RGB(102, 255, 102));
+    cleardevice();
+    settextstyle(3,HORIZ_DIR,4);
+    setcolor(4);
+    outtextxy(250,10,"Simulacion del juego de Torres de Hanoi");
 
 
+
+    settextstyle(3,HORIZ_DIR,4);
+    setcolor(2);
+    outtextxy(200,450,"Torre A");
+
+    settextstyle(3,HORIZ_DIR,4);
+    setcolor(2);
+    outtextxy(500,450,"Torre B");
+
+    settextstyle(3,HORIZ_DIR,4);
+    setcolor(2);
+    outtextxy(800,450,"Torre C");
+
+    setcolor(0);
+    line(250,150,250 ,420);
+    line(550,150,550 ,420);
+    line(850,150,850 ,420);
+
+
+getch();
+closegraph();
+
+
+
+return 0;
 }
 
 
